85a29bbce7c10b1be1a89b64a083ef6841fa2c23
slower-s <slowersis@onitkorea.com>
2021-10-20 14:11:53 +0900

    chore: meta tag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,8 @@ class MyDocument extends Document {
           <meta property="og:title" content="회사 주변 맛집 찾기"></meta>
           <meta property="og:description" content="회사 주변 반경 500m 맛집 크롤링 후 Notion에 적용까지!"></meta>
           <meta property="og:image" content="https://post-phinf.pstatic.net/MjAyMDEwMzBfMjU1/MDAxNjA0MDMyMjkxODU3.mkQ1XS3WDGYl4Hil2GrXCENETyYUwDg3UTfKHvpDQy8g.fsLh_3fZ7SieqRbkCjXtrRv8pKWd0oHndCcEvY9S6wMg.PNG/4.png?type=w1200"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:url" content="https://food-crawl.vercel.app/"></meta>
         </Head>
         <body>
           <Main />
